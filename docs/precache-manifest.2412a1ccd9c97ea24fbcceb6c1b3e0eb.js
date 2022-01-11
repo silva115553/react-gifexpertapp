ed81a57492e7079adeaae570666ccb20ef0da7a1
Arturo Silva <arturosilva.uni@gmail.com>
2022-01-11 20:13:02 +0100

    docs agregado

---
+++ docs/precache-manifest.2412a1ccd9c97ea24fbcceb6c1b3e0eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9244e597beb5c9b00c136bd811283547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c5c33067eb19c5fe635",
+    "url": "/static/css/main.8be25f7b.chunk.css"
+  },
+  {
+    "revision": "7d51ea710445724531a2",
+    "url": "/static/js/2.c5268ded.chunk.js"
+  },
+  {
+    "revision": "9c5c33067eb19c5fe635",
+    "url": "/static/js/main.4fff0f2c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
